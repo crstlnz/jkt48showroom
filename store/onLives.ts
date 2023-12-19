1d1d14978787a2ac0bec7ff86a70ae933896d677
Crytalenz <kelvinzzzzzzzzzz@gmail.com>
2023-12-19 23:38:39 +0700

    fix lives missing

--- store/onLives.ts
+++ store/onLives.ts
@@ -1,3 +1,4 @@
+import { useNotifications } from './notifications'
 import { useSettings } from './settings'
 import JSONSerializer from '~~/library/serializer/json'
 
@@ -12,7 +13,7 @@ export const useOnLives = defineStore('onLives', () => {
   const settings = useSettings()
 
   const hasLives = computed(() => {
-    return (lives.value?.showroom?.length ?? lives.value?.idn?.length ?? 0) > 0
+    return (lives.value?.showroom?.length || lives.value?.idn?.length || 0) > 0
   })
 
   const liveCount = computed(() => (lives.value?.showroom?.length ?? 0) + (lives.value?.idn?.length ?? 0))
@@ -37,30 +38,35 @@ export const useOnLives = defineStore('onLives', () => {
   }
 
   async function getShowroomLives(): Promise<IRoomLive[]> {
-    if (!config.public.isDev) {
-      return await $apiFetch(`/api/now_live`, { query: { group: settings.group, _: new Date().getTime() } })
-    }
-    else {
-      const data: any = await $apiFetch(`/api/showroom/onlives`)
-      const re = (data?.onlives[0]?.lives ?? []).splice(0, 3).map((i: any) => {
-        return {
-          name: i.main_name ?? 'Test name',
-          img: i.image ?? 'https://static.showroom-live.com/image/room/cover/ee38ccf437e220f7ce8149c1c8aac94d6dca66734334bdad84c94bf41e78d3e0_square_s.png?v=1670924861',
-          url: i.room_url_key ?? '',
-          room_id: i.room_id ?? 0,
-          is_graduate: false,
-          is_group: false,
-          room_exists: true,
-          streaming_url_list: i.streaming_url_list ?? [],
-          started_at: i.started_at * 1000,
-        }
-      })
-      return [...await $apiFetch<IRoomLive[]>(`/api/now_live`), ...re]
-    }
+    return await $apiFetch(`/api/now_live`, { query: { group: settings.group, _: new Date().getTime() } })
+    // if (!config.public.isDev) {
+    // }
+    // else {
+    //   const data: any = await $apiFetch(`/api/showroom/onlives`)
+    //   const re = (data?.onlives[0]?.lives ?? []).splice(0, 3).map((i: any) => {
+    //     return {
+    //       name: i.main_name ?? 'Test name',
+    //       img: i.image ?? 'https://static.showroom-live.com/image/room/cover/ee38ccf437e220f7ce8149c1c8aac94d6dca66734334bdad84c94bf41e78d3e0_square_s.png?v=1670924861',
+    //       url: i.room_url_key ?? '',
+    //       room_id: i.room_id ?? 0,
+    //       is_graduate: false,
+    //       is_group: false,
+    //       room_exists: true,
+    //       streaming_url_list: i.streaming_url_list ?? [],
+    //       started_at: i.started_at * 1000,
+    //     }
+    //   })
+    //   return [...await $apiFetch<IRoomLive[]>(`/api/now_live`), ...re]
+    // }
   }
 
+  const { addNotif } = useNotifications()
   async function getIDNLives(): Promise<IDNLives[]> {
     return await $apiFetch<IDNLives[]>(`/api/idn_lives`).catch((_) => {
+      addNotif({
+        type: 'danger',
+        message: 'Failed to get IDN Lives Data!',
+      })
       return []
     })
   }
