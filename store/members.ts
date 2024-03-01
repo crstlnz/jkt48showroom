2e2c3df32f0f9fd7954797d54fb7a1c21b4f10cf
Crytalenz <kelvinzzzzzzzzzz@gmail.com>
2024-03-02 03:50:02 +0700

    Update members.ts

--- store/members.ts
+++ store/members.ts
@@ -4,7 +4,7 @@ export const useMembers = defineStore('members', () => {
   const error = ref<Error | null>(null)
   const loading = ref(false)
   const settings = useSettings()
-  const { data: members, trySet, isValid } = useExpiredLocalStorage<IMember[]>(`membersv5-${settings.group}`, 86400000)
+  const { data: members, trySet, isValid } = useExpiredLocalStorage<IMember[]>(`membersv6-${settings.group}`, 86400000)
   async function fetch() {
     await trySet(async () => await $apiFetch('/api/member', { query: { group: settings.group, _: 'v5' } }))
   }
