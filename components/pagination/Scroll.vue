3ac0540ee330120859620a64e9d42c4854499429
Crstlnz <52141479+crstlnz@users.noreply.github.com>
2024-07-15 17:07:39 +0700

    Update Scroll.vue

--- components/pagination/Scroll.vue
+++ components/pagination/Scroll.vue
@@ -117,7 +117,7 @@ watch(res, (val) => {
         >
           <button
             type="button"
-            class="flex-1 border-r border-second-2 px-2 py-2.5 text-center active:bg-blue-500"
+            class="flex-1 border-r border-second-2 px-2 py-2.5 text-center active:bg-blue-500 justify-center flex gap-1 items-center"
             @click="scrollToTop"
           >
             {{ $t("scrolltop") }} <Icon name="ph:arrow-up-bold" class="self-center text-base" />
