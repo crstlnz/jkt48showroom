42dd7f3733aa5b71e735a23d9fcca42bec10289b
Crytalenz <kelvinzzzzzzzzzz@gmail.com>
2024-09-01 19:59:40 +0700

    Update 2024Label.vue

--- components/sousenkyo/2024Label.vue
+++ components/sousenkyo/2024Label.vue
@@ -1,6 +1,6 @@
 <template>
-  <div class="bg-blue-600 px-1 py-0.5 rounded-sm flex gap-1 items-center border-yellow-400/50 border">
-    <Icon name="mdi:crown-outline" class="text-yellow-400 text-base" /><div class="text-xs font-bold">
+  <div class="bg-gradient-to-r from-pink-400 via-purple-400 to-cyan-400 px-1 py-0.5 rounded-md flex gap-1 items-center border-yellow-300/50 border">
+    <Icon name="mdi:crown-outline" class="text-yellow-300 text-base" /><div class="text-xs font-bold">
       Sousenkyo 2024
     </div>
   </div>
